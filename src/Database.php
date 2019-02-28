8bf6944f98cd236170daa5e1c406aae58b511d28
Faks <sia_dev@inbox.lv>
2019-02-28 10:44:58 +0200

    Final Commit

--- src/Database.php
+++ src/Database.php
@@ -13,7 +13,7 @@
 	 */
 	
 	define('DB_HOST', '173.249.33.8');
-	define('DB_NAME', 'mtc');
-	define('DB_USER', 'mtc');
-	define('DB_PASSWORD', 'bWwZSTMe4odan3nb');
+	define('DB_NAME', 'mtc_trial');
+	define('DB_USER', 'mtc_trial');
+	define('DB_PASSWORD', '3Iq6FyKzuLZ6Un5M');
 	define('DB_PORT', 3306);
